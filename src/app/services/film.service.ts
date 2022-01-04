9861888dcce5aea786ab72693b149a001ca40742
HadilAbdennebi <77753409+HadilAbdennebi@users.noreply.github.com>
2022-01-04 01:00:15 +0100

    Update film.service.ts

--- src/app/services/film.service.ts
+++ src/app/services/film.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Film } from '../model/film';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from './auth.service';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -12,71 +13,53 @@ const httpOptions = {
 })
 export class FilmService {
   apiURL: string = 'http://localhost:8092/films/api';
-  films! : Film[];
+  films!: Film[];
   film: Film;
-  constructor(private http: HttpClient) {
-    /*
-    
-    this.films = [
-      {idFilm: 1, nomFilm: "Titanic123", prixTicket: 3000.600, dateSortie : new Date("01/14/2011") },
-      {  idFilm: 2, nomFilm: "Hush", prixTicket: 450, dateSortie: new Date("12/17/2010")},
-      { idFilm: 3, nomFilm: "Soul", prixTicket: 900.123, dateSortie: new Date("02/20/2020")},
-      { idFilm: 4, nomFilm: "Frozen", prixTicket: 900.123, dateSortie: new Date("02/20/2020")}
+  //apiURL: string = 'http://localhost:8081/api';
 
-    ];
-    */
+  constructor(private http: HttpClient, private authService: AuthService) {
+  
   }
-  /*
-    listeFilms(): Film[] {
-      return this.films;
-    }
-  */
+  
 
   listeFilms(): Observable<Film[]> {
-    return this.http.get<Film[]>(this.apiURL);
+
+    let jwt = this.authService.getToken();
+     jwt = "Bearer "+jwt; 
+     let httpHeaders = new HttpHeaders({"Authorization":jwt})
+      return this.http.get<Film[]>(this.apiURL,{headers:httpHeaders});
   }
-/*
-  ajouterFilm(f: Film) {
-    this.films.push(f);
+
+
+  ajouterFilm(prod: Film): Observable<Film> {
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt})
+    return this.http.post<Film>(this.apiURL, prod, {headers:httpHeaders});
   }
-  */
-  ajouterFilm( prod: Film):Observable<Film>{
-    return this.http.post<Film>(this.apiURL, prod, httpOptions);
+  supprimerFilm(id: number) {
+    const url = `${this.apiURL}/${id}`;
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt})
+    return this.http.delete(url, {headers:httpHeaders});
+  }
+
+  consulterFilm(id: number): Observable<Film> {
+    const url = `${this.apiURL}/${id}`;
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt})
+    return this.http.get<Film>(url,{headers:httpHeaders});
+  }
+ 
+  updateFilm(prod: Film): Observable<Film> {
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt})
+    return this.http.put<Film>(this.apiURL, prod, {headers:httpHeaders});
     }
-    supprimerFilm(id : number) {
-      const url = `${this.apiURL}/${id}`;
-      return this.http.delete(url, httpOptions);
-      }
-/*      
-  supprimerFilm(f: Film) {
-    //supprimer le Film prod du tableau Films
-    const index = this.films.indexOf(f, 0);
-    if (index > -1) {
-      this.films.splice(index, 1);
-    }
-    //ou Bien
-    /* this.Films.forEach((cur, index) => {
-    if(prod.idFilm === cur.idFilm) {
-    this.Films.splice(index, 1);
-    }
-    }); */
- /* }
-*/
-consulterFilm(id: number): Observable<Film> {
-  const url = `${this.apiURL}/${id}`;
-  return this.http.get<Film>(url);
-  }
-  /*
-  consulterFilm(id: number): Film {
-    this.film = this.films.find(p => p.idFilm == id);
-    return this.film;
-  }
-*/
-  updateFilm (prod :Film) : Observable<Film>
-  {
-  return this.http.put<Film>(this.apiURL, prod, httpOptions);
-  }
-   
+
 
   trierFilms() {
     this.films = this.films.sort((n1, n2) => {
@@ -89,5 +72,6 @@ consulterFilm(id: number): Observable<Film> {
       return 0;
     });
   }
+
 
 }

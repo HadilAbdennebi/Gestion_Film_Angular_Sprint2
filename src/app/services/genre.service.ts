f9863c244f1b380245909b8a4e1eaa623bb3c267
HadilAbdennebi <77753409+HadilAbdennebi@users.noreply.github.com>
2022-01-04 01:00:49 +0100

    Update genre.service.ts

--- src/app/services/genre.service.ts
+++ src/app/services/genre.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Genre } from '../model/genre';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from './auth.service';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
@@ -16,29 +17,45 @@ export class GenreService {
   apiURL: string = 'http://localhost:8092/films/api/genre';
   genres! : Genre[];
   genre: Genre;
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private authService: AuthService) {
   }
   listeGenres(): Observable<Genre[]> {
-    return this.http.get<Genre[]>(this.apiURL);
-  }
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt; 
+    let httpHeaders = new HttpHeaders({"Authorization":jwt})
+     return this.http.get<Genre[]>(this.apiURL,{headers:httpHeaders});  }
+ 
 
   ajouterGenre( prod: Genre):Observable<Genre>{
-    return this.http.post<Genre>(this.apiURL, prod, httpOptions);
-    }
-    supprimerGenre(id : number) {
-      const url = `${this.apiURL}/${id}`;
-      return this.http.delete(url, httpOptions);
-      }
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt})
+    return this.http.post<Genre>(this.apiURL, prod, {headers:httpHeaders});    }
+  
+   
+   supprimerGenre(id : number) {
+    const url = `${this.apiURL}/${id}`;
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt})
+    return this.http.delete(url, {headers:httpHeaders});
+  }
 
 consulterGenre(id: number): Observable<Genre> {
   const url = `${this.apiURL}/${id}`;
-  return this.http.get<Genre>(url);
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt})
+    return this.http.get<Genre>(url,{headers:httpHeaders});
+
   }
 
   updateGenre(prod :Genre) : Observable<Genre>
   {
-  return this.http.put<Genre>(this.apiURL, prod, httpOptions);
-  }
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt})
+    return this.http.put<Genre>(this.apiURL, prod, {headers:httpHeaders});  }
  
   
   trierGenres() {

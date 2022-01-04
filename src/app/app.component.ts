a0795c552b67d2c8820ad3b36214090cff1457f7
HadilAbdennebi <77753409+HadilAbdennebi@users.noreply.github.com>
2022-01-04 01:03:53 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'films';
+
+  constructor (public authService: AuthService, private router : Router) {}
+
+  onLogout(){
+    this.authService.logout();
+  }
+
+  ngOnInit () {
+    this.authService.loadToken();
+    if (this.authService.getToken()==null ||
+     this.authService.isTokenExpired())
+    this.router.navigate(['/login']);
+  }    
 }
+
+
